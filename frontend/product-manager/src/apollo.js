2216f8e8feeaabec4a51f5ebb90c1dd6b4d56a1a
sithandekile <sthandie15@gmail.com>
2026-08-18 22:54:40 +0200

    getting product to display on app.jsx

---
+++ frontend/product-manager/src/apollo.js
@@ -0,0 +1,16 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
---
+++ frontend/product-manager/src/graphql/queries.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      name
+      description
+      price
+      category
+    }
+  }
+`;
